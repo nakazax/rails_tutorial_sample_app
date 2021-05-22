342555f7602c45825390d5b9a3cabb0606ce0c1a
Hiroyuki Nakazato <nakazato.hiroyuki@gmail.com>
2021-05-22 05:41:56 +0000

    11.3.1 終了、ただしテストでエラーあり

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,6 +34,9 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
+  host = '21e24e74bb1d41c5aea954f5fd2cba49.vfs.cloud9.ap-northeast-1.amazonaws.com'
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
