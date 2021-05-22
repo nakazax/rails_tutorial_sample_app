775fa8af9d5cd800a3213959c053f3a6f4460c6d
Hiroyuki Nakazato <nakazato.hiroyuki@gmail.com>
2021-05-22 07:19:06 +0000

    13.2.2 完了後、ただしエラーあり

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
 
   def index
     @users = User.paginate(page: params[:page])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def show
